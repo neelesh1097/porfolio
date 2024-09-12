6778d5d03958a146f9f89b0c4ee17fd82b26f3e2
neelesh1097 <neelesh6kumar@gmail.com>
2024-09-12 22:03:48 +0530

    s

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ import Nav from "./components/Nav";
 
 import Home from "./components/Home";
 import About from "./components/About";
-import Project from "./components/project";
+
 
 import Contact from "./components/Contact";
 import Footer from "./components/footer";
+import Project from "./components/Project";
 
 function App() {
  
